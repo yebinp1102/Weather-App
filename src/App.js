e26c0eeeed4916d5e0c260892058c385b9ee495a
yebin Park <yebinp1102@naver.com>
2022-02-15 23:19:00 +0900

    main setting of weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const apiKey = 'f24a53eac0adac173f52b6d010058a94';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=${apiKey}`
+
+  return(
+    <Container>
+      Weather App
+    </Container>
+  )
 }
 
 export default App;
+
+const Container = styled.div`
+
+`;
